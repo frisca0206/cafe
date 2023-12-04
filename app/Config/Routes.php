63629b1c38d003dbe0e7eaccc827f882939057f6
PKL (GLA) <friscafebrianti16@gmail.com>
2023-12-04 09:29:44 +0700

    add layouts

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// => default controller
+$routes->get('dashboard', 'DashboardController::index',['as' => 'dashboard']);
